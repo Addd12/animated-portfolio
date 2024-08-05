28d1e635628331bc65a2bbc7825a6defa1757dac
Addd12 <addd.github@gmail.com>
2024-08-05 21:14:24 +0200

    added components folder and app.scss

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.scss"
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        {/* Sidebar */}
+        <div className="wrapper">
+            <span>Eneada Sulaj</span>
+        </div>
+    </div>
+  )
+}
