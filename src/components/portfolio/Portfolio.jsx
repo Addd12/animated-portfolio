b7aa0fb199dd1d35bf9f1f06ca429da3efdff5a4
Addd12 <addd.github@gmail.com>
2024-08-26 20:06:15 +0200

    added featured works with animations

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import "./portfolio.scss";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
@@ -34,7 +34,29 @@ const items = [
 ];
 
 const Single = ({ item }) => {
-  return <section>{item.title}</section>;
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    //offset: ["start start", "end start"]
+  });
+
+  const y = useTransform(scrollYProgress, [0,1], [-300, 300]);
+
+  return <section>
+    <div className="container">
+      <div className="wrapper">
+        <div className="imageContainer">
+          <img src={item.img} alt="" ref={ref}/>
+        </div>
+        <motion.div className="textContainer" style={{y}}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          <button>See demo</button>
+        </motion.div>
+        </div>
+    </div>
+    </section>;
 };
 
 const Portfolio = () => {
