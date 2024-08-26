d999cd87b1656350a43de09405874372c615a945
Addd12 <addd.github@gmail.com>
2024-08-26 22:25:11 +0200

    added contact component with svg animation

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,92 @@
+import { useRef } from "react";
+import "./contact.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Contact = () => {
+    const ref = useRef()
+
+    const isInView = useInView(ref, {margin:"-100px"})
+    
+  return (
+    <motion.div
+    ref={ref}
+      className="contact"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div className="textContainer">
+        <motion.h1 variants={variants}>Let's work together</motion.h1>
+        <motion.div className="item" variants={variants}>
+          <h2>Mail</h2>
+          <span>ada@example.com</span>
+        </motion.div>
+        <motion.div className="item" variants={variants}>
+          <h2>Address</h2>
+          <span>Street and number</span>
+        </motion.div>
+        <motion.div className="item" variants={variants}>
+          <h2>Phone</h2>
+          <span>00 111 222 333</span>
+        </motion.div>
+      </motion.div>
+      <div className="formContainer">
+        <motion.div
+          className="phoneSvg"
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 0 }}
+          transition={{ delay: 3, duration: 1 }}
+        >
+          <svg
+            width="450px"
+            height="450px"
+            viewBox="0 0 64 64"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+          >
+            <motion.path
+              initial={{ pathLength: 0 }}
+              whileInView={{ pathLength: 1 }}
+              transition={{ duration: 2 }}
+              d="M53.26 54.62a4.09 4.09 0 0 0 2.51-5.22l-1.36-3.87A4.1 4.1 0 0 0 49.18 43c-7.73 2.71-10.45-5-11.81-8.88S33.3 22.55 41 19.83a4.1 4.1 0 0 0 2.51-5.22l-1.36-3.87A4.1 4.1 0 0 0 37 8.23c-9.66 3.4-14.1 9.3-7.31 28.63S43.6 58 53.26 54.62z"
+            />
+            <motion.path
+              initial={{ pathLength: 0 }}
+              animate={isInView && { pathLength: 1 }}
+              transition={{ duration: 2 }}
+              d="M34.81 48.18A4 4 0 0 0 32 52a4 4 0 0 1-8 0 4 4 0 0 0-8 0 4 4 0 0 1-8 0"
+            />
+          </svg>
+        </motion.div>
+        <motion.form
+          action=""
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 4, duration: 1 }}
+        >
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea rows={8} placeholder="Message"></textarea>
+          <button>Submit</button>
+        </motion.form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;
